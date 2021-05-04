3225016044b91e17f5c57b8a6f6a793f79cd931d
hanezu <tim.zhc@gmail.com>
2021-05-04 20:46:42 +0800

    update

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
-const {recommendRestaurant} = require('./recommend')
-
-recommendRestaurant()
+exports.helloWorld = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
---
+++ run.ts
@@ -0,0 +1,4 @@
+const {recommendRestaurant} = require('./recommend')
+
+recommendRestaurant()
+
